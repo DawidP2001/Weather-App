f195cbbd5c0e1a2bff50112ad76bfe41df7a50b8
Dawidp2001 <Dawidpionk01@gmail.com>
2025-07-09 15:40:11 +0100

    Started working on the carousel for the daily forecasts

--- weather-app/src/components/DayForecast.jsx
+++ weather-app/src/components/DayForecast.jsx
@@ -1,7 +1,10 @@
+import Carousel from "./carousel";
+
 // This componenent is responsible for displaying summary for a single day of weather forecast.
 export default function dayForecast({weatherForecast, setSelectedDay}) {
     const conditionText = weatherForecast.day.condition?.text || "Condition not available";
     return (
+    <>
         <button onClick={() => setSelectedDay(weatherForecast)} className="m-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
@@ -14,5 +17,6 @@ export default function dayForecast({weatherForecast, setSelectedDay}) {
                 <p className="whitespace-nowrap overflow-hidden overflow-ellipsis"></p>
             </div>
         </button>
+    </>
     );
 }
